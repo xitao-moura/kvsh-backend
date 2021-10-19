95259c986d6c8655ac28159c130d16cf02e381e3
Xitão Moura <washington.moura@gnis.com.br>
2021-10-19 09:41:52 -0300

    feat: first commit

--- config/database.js
+++ config/database.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => ({
         srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'backend'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        username: env('DATABASE_USERNAME', 'kvsh'),
+        password: env('DATABASE_PASSWORD', 'rewq987aa'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
