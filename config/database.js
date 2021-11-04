4c432f2c0b0164c05c221c2d9a747c058cdc3f96
Xitão Moura <washington0moura@gmail.com>
2021-11-03 22:17:07 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => ({
         srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'kvsh'),
-        username: env('DATABASE_USERNAME', 'kvsh'),
-        password: env('DATABASE_PASSWORD', 'kvsh@!'),
+        //username: env('DATABASE_USERNAME', 'kvsh'),
+        //password: env('DATABASE_PASSWORD', 'kvsh@!'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
